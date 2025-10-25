6d41a1adbbccc1ff2e112c846772c3f90043ab01
Andre <andremuzel@gmail.com>
2025-10-25 01:12:38 -0300

    change fs readonly

--- setup-app.sh
+++ setup-app.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
 # Script para configurar uma nova aplicação usando este template
-# Uso: ./setup-app.sh <app-name> <repo-url> <image-repo> <domain>
+# Uso: ./setup-app.sh <app-name> <repo-url> <image-repo> <image-tag> <domain>
 
 set -e
 
-if [ $# -ne 4 ]; then
-    echo "Uso: $0 <app-name> <repo-url> <image-repo> <domain>"
-    echo "Exemplo: $0 minha-app https://github.com/user/repo.git myregistry/myapp example.com"
+if [ $# -ne 5 ]; then
+    echo "Uso: $0 <app-name> <repo-url> <image-repo> <image-tag> <domain>"
+    echo "Exemplo: $0 minha-app https://github.com/user/repo.git myregistry/myapp latest example.com"
     exit 1
 fi
 
 APP_NAME=$1
 REPO_URL=$2
 IMAGE_REPO=$3
-DOMAIN=$4
+IMAGE_TAG=$4
+DOMAIN=$5
 
 echo "Configurando aplicação: $APP_NAME"
 echo "Repositório: $REPO_URL"
 echo "Imagem: $IMAGE_REPO"
+echo "Tag da imagem: $IMAGE_TAG"
 echo "Domínio: $DOMAIN"
 
 # Escapar caracteres especiais para sed
@@ -31,6 +33,7 @@ echo "Substituindo variáveis nos arquivos..."
 find . -type f \( -name "*.yaml" -o -name "*.yml" \) -exec sed -i "s/APP_NAME/$APP_NAME/g" {} \;
 find . -type f \( -name "*.yaml" -o -name "*.yml" \) -exec sed -i "s|REPO_URL|$ESCAPED_REPO_URL|g" {} \;
 find . -type f \( -name "*.yaml" -o -name "*.yml" \) -exec sed -i "s|IMAGE_REPOSITORY|$ESCAPED_IMAGE_REPO|g" {} \;
+find . -type f \( -name "*.yaml" -o -name "*.yml" \) -exec sed -i "s/IMAGE_REPOSITORY_TAG/$IMAGE_TAG/g" {} \;
 find . -type f \( -name "*.yaml" -o -name "*.yml" \) -exec sed -i "s/DOMAIN/$DOMAIN/g" {} \;
 
 echo "Configuração concluída!"
